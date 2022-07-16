01df763c33353d1254e674c38ece6b04ec33856c
Maksim Kushnirov <maks.kushnirov@gmail.com>
2022-07-16 23:46:40 +0300

    add a promotion and a like

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,13 +1,19 @@
+import {useState} from "react";
 import "./employees-list-item.scss";
 
-function EmployeesListItem({name, salary, promotion}){
-    const className = `${promotion ? "promotion" : ""} list-group-item d-flex justify-content-between`
+function EmployeesListItem({name, salary, promotionP}){
+    const [promotion, setPromotion] = useState(promotionP);
+    const [like, setLike] = useState(false);
+
+    const className = `${promotion ? "promotion" : ""} 
+                        ${like ? "like" : ""} 
+                        list-group-item d-flex justify-content-between`
     return (
         <li className={className}>
-            <span className="list-group-item-label">{name}</span>
+            <span onClick={() => setLike(!like)} className="list-group-item-label">{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary+"$"}/>
             <div className="d-flex justify-content-center align-items-center">
-                <button className="btn-cookie btn-sm">
+                <button onClick={() => setPromotion(!promotion)} className="btn-cookie btn-sm">
                     <i className="fas fa-cookie"></i>
                 </button>
                 <button className="btn-trash btn-sm">
