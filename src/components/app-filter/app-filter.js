257f07929b60caeb3c856430880025038cab5d19
Maksim Kushnirov <maks.kushnirov@gmail.com>
2022-07-18 16:44:33 +0300

    clear code

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,21 +1,21 @@
 import "./app-filter.scss"
 
 function AppFilter({setFilter, currentFilter}){
-    const handleClick = e => {
-        setFilter(e);
-    }
-    
-    const buttons = [
+    const buttonsData = [
         {name: "All", filter: "all"},
         {name: "Promotion", filter: "promo"},
         {name: "Salary >1000$", filter: "salary"},
-    ].map(({name, filter}) => (
-        <button onClick={handleClick} 
-                data-filter={filter} 
-                className={`btn ${currentFilter === filter ? "btn-light" : "btn-outline-light"}`}
-                key={name}
-                >{name}</button>
-    ));
+    ]
+    const buttons = buttonsData.map(({name, filter}) => {
+        const clazz = currentFilter === filter ? "btn-light" : "btn-outline-light";
+        return <button 
+                    onClick={setFilter} 
+                    data-filter={filter} 
+                    className={`btn ${clazz}`}
+                    key={name}>
+                    {name}
+                </button>
+    });
 
     return (
         <div className="app-filter">
